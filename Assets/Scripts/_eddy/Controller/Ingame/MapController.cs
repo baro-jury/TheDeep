b100d468b175a477484538d21d7ff9038817a44b
MangoGamesVN <154311688+MangoGamesVN@users.noreply.github.com>
2024-09-23 17:54:55 +0700

    update map

--- Assets/Scripts/_eddy/Controller/Ingame/MapController.cs
+++ Assets/Scripts/_eddy/Controller/Ingame/MapController.cs
@@ -10,7 +10,7 @@ public class MapController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(roomNormal, transform);
     }
 
     
